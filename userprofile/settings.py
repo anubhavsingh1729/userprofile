c93dc119a3f26d8d9d612655ec0c997211fd10a9
ryan10 <ryansimgh7417@gmail.com>
2019-06-01 16:16:24 +0530

    changes required for heroku

--- userprofile/settings.py
+++ userprofile/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'fierce-tor-76419.herokuapp.com',
 ]
 
 ROOT_URLCONF = 'userprofile.urls'
